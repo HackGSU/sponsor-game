7eda412f084ed9d66440f2d2cb4fc845bf795f89
Abhinav Reddy <reddy@Abhinavs-MacBook-Pro.local>
2016-09-29 16:01:37 -0400

    Started working on the Space Invader Sponsors Game

---
+++ js/helpers.js
@@ -0,0 +1,45 @@
+// Helper functions
+
+// Screen
+function Screen(width, height) {
+  this.canvas = document.createElement("canvas");
+  this.canvas.width = this.width = width;
+  this.canvas.height = this.height = height;
+  this.ctx = this.canvas.getContext("2d");
+
+  document.body.appendChild(this.canvas);
+};
+
+Screen.prototype.drawSprite = function(sp, x, y) {
+  this.ctz.drawImage(sp.img, sp.x, sp.y, sp.w, sp.h, x, y, sp.w, sp.h);
+};
+// Sprite
+function Sprite(img, x, y, w, h) {
+  this.img = img;
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+};
+// Input Handler
+function InputHandler() {
+  this.down = {};
+  this.pressed = {};
+
+  var _this = this;
+  document.addEventListener("keydown", function(evt) {
+    _this.down[evt.keyCode] = true;
+  });
+  document.addEventListener("keyup", function(evt) {
+    delete _this.down[evt.keyCode];
+    delete _this.pressed[evt.keyCode];
+  });
+};
+
+InputHandler.prototype.isDown = function(code) {
+
+};
+
+InputHandler.prototype.isPressed = function(code) {
+
+};
