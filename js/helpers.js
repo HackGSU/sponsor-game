a6a242e12b4148e7d1e373dd2782351e01331635
Abhinav Reddy <reddy@Abhinavs-MacBook-Pro.local>
2016-10-11 16:23:47 -0400

    Added projectile attacks from aliens. Woot

--- js/helpers.js
+++ js/helpers.js
@@ -1,5 +1,9 @@
 // Helper functions
 
+function AABBIntersect(ax, ay, aw, ah, bx, by, bw, bh) {
+  return ax < bx+bw && bx < ax+aw && ay < by+bh && by < ay+ah;
+};
+
 //Projectile
 function Projectile(x, y, vely, w, h, color) {
   this.x = x;
